aa70302bff48ab5a5af1dd6b72c9a6428ab92689
ozzaynetworks <ozzaynetworks@gmail.com>
2022-07-17 23:04:21 +0300

    kelvin: payment modal timer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -64,16 +64,63 @@ $(window).on('load', function() {
 
     //sending payment
     $('body').on('click','.payment-next', function(){
+
+        $('.payment-prev').prop('disabled', false)       
+        var countTheSteps = parseFloat($(".payment-panel-parent .payment-panel").length);
+        var theCurrentIndex= $(".payment-panel-parent .payment-panel.payment-active-panel").index();
+        theCurrentIndex=theCurrentIndex+2;
+        if(theCurrentIndex!=countTheSteps){
+            $('.payment-panel-parent').find('.payment-active-panel').addClass('d-none').removeClass('payment-active-panel').next().removeClass('d-none').addClass('payment-active-panel')
+        }
+        if (theCurrentIndex==countTheSteps-1) {
+            $('.payment-next').addClass('d-none');
+        }
+
+        // else{
+        //     $('.payment-next').addClass('d-none'); 
+        // }
+          
         
-        $('.payment-panel-parent').find('.payment-active-panel').addClass('d-none').removeClass('payment-active-panel').next().removeClass('d-none').addClass('payment-active-panel')
 
     })
 
     $('body').on('click','.payment-prev', function(){
+        $('.payment-next').removeClass('d-none');  
+        var countTheSteps = parseFloat($(".payment-panel-parent .payment-panel").length);
+        var theCurrentIndex= $(".payment-panel-parent .payment-panel.payment-active-panel").index();
+        if(theCurrentIndex!=0){
+            $('.payment-panel-parent').find('.payment-active-panel').addClass('d-none').removeClass('payment-active-panel').prev().removeClass('d-none').addClass('payment-active-panel')
+        }   
         
-        $('.payment-panel-parent').find('.payment-active-panel').addClass('d-none').removeClass('payment-active-panel').prev().removeClass('d-none').addClass('payment-active-panel')
 
+    });
+    $('body').on('click', '.btn-send', function(){
+        $('#walletTopUp .modal-footer').addClass('d-none');
+        $('#walletTopUp .close-modal').addClass('d-none');
+        $('.stk-timer-container').removeClass('d-none').siblings().addClass('d-none');
+        $(this).prop('disabled', true);
+
+        var timeleft = 5;
+        var downloadTimer = setInterval(function () {
+            if (timeleft <= 0) {
+                clearInterval(downloadTimer);
+                alert("You took to long to confirm the transaction");
+                $(".stk-timer").text("0 s");
+                $('.btn-send').prop('disabled', false);
+                $('#walletTopUp .close-modal').removeClass('d-none');
+
+                $('#walletTopUp .modal-footer').removeClass('d-none');
+                $('.stk-timer-container').addClass('d-none').siblings().removeClass('d-none');
+            }
+
+            $(".stk-timer").text(timeleft + " s");
+                timeleft -= 1;
+               
+            }, 1000);
     })
+    
+
+    
 
 });
 
